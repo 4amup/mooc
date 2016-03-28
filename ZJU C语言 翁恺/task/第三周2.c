fc6833d84701f5fb03b80501aea1768002e8eac0
wozhizuimac <490398311@qq.com>
2016-03-28 22:59:56 +0800

    task 2 C语言

--- ZJU C语言 翁恺/task/第三周2.c
+++ ZJU C语言 翁恺/task/第三周2.c
@@ -3,24 +3,12 @@
 int main()
 {
 	int number;
+	int arr[10];
+	int 
 	scanf("%d", &number);
-	int temp = number;
-	int digit_p = 0;
-	int reslut = 0;
-	do
+	if (number >= 0 && number <=100000)
 	{
-		int digit = temp % 10;
-		temp %=10;
-		digit_p++;
-		int a = digit % 2;
-		int b = digit_p % 2;
-		if (a !=0 & b! =0) or (a == 0 & b ==0)
-		{
-			reslut = 1 * 2 ** digit_p;
-		}
-		else
-		{
-			reslut = 
-		}
+		/* code */
 	}
- } 
+	return 0;
+} 
