35ce30084d4f5b0537c468353a6edff9b1913812
wozhizuipc <wozhizui1990@163.com>
2016-05-30 08:38:16 +0800

    revert chater 4

--- Web/DOM Scripting/chapter06/image_gallery/01/scripts/showPic.js
+++ Web/DOM Scripting/chapter06/image_gallery/01/scripts/showPic.js
@@ -5,40 +5,39 @@ function showPic(whichpic) {
   placeholder.setAttribute("src",source);
   if (!document.getElementById("description")) return false;
   if (whichpic.getAttribute("title")) {
-    var text = whichpic.getAttribute("title");
+	var text = whichpic.getAttribute("title");
   } else {
-    var text = "";
+	var text = "";
   }
   var description = document.getElementById("description");
   if (description.firstChild.nodeType == 3) {
-    description.firstChild.nodeValue = text;
+	description.firstChild.nodeValue = text;
   }
   return false;
 }
 
 function prepareGallery() {
-  if (!document.getElementsByTagName) return false;
-  if (!document.getElementById) return false;
+  if (!document.getElementsByTagName||!document.getElementById) return false;
   if (!document.getElementById("imagegallery")) return false;
   var gallery = document.getElementById("imagegallery");
   var links = gallery.getElementsByTagName("a");
   for ( var i=0; i < links.length; i++) {
-    links[i].onclick = function() {
-      return showPic(this);
+	links[i].onclick = function() {
+	  return showPic(this);
 	}
-    links[i].onkeypress = links[i].onclick;
+	links[i].onkeypress = links[i].onclick;
   }
 }
 
 function addLoadEvent(func) {
   var oldonload = window.onload;
   if (typeof window.onload != 'function') {
-    window.onload = func;
+	window.onload = func;
   } else {
-    window.onload = function() {
-      oldonload();
-      func();
-    }
+	window.onload = function() {
+	  oldonload();
+	  func();
+	}
   }
 }
 
