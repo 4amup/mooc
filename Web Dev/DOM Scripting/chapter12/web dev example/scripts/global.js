c3b014a0b103ea638440618eb5ad34522f07bb5f
wozhizuipc <wozhizui1990@163.com>
2016-06-15 16:43:49 +0800

    over 12.5.1
    页面突出显示
    update highlightpage函数
    在每个page的body元素添加了类，用来替换默认的header元素背景图案。

--- Web Dev/DOM Scripting/chapter12/web dev example/scripts/global.js
+++ Web Dev/DOM Scripting/chapter12/web dev example/scripts/global.js
@@ -27,4 +27,26 @@ function addClass(element,value){
 		element.className = newClassName;
 	}
 }
-以上是常用的几个功能性函数，是通用的
+// 以上是常用的几个功能性函数，是通用的
+function highLightPage() {
+	// 以上是检查正确性的代码，并取得nav元素数组（虽然只有一个）
+	if (!document.getElementsByTagName) {return false};
+	if (!document.getElementById) {return false};
+	var headers = document.getElementsByTagName("header");
+	if (headers.length == 0) {return false};
+	var navs = headers[0].getElementsByTagName("nav");
+	if (navs.length == 0) {return false};
+	// 取得链接数组，
+	var links = navs[0].getElementsByTagName("a");
+	var linkurl;
+	// 循环对比找到是本页面的地址，添加here类
+	for (var i = 0; i < links.length; i++) {
+		linkurl = links[i].getAttribute("href");
+		if (window.location.href.indexOf(linkurl) != -1) {
+			links[i].className = "here";
+			var linktext = links[i].lastChild.nodeValue.toLowerCase();
+			document.body.setAttribute("id",linktext);
+		}
+	}
+}
+addLoadEvent(highLightPage);
