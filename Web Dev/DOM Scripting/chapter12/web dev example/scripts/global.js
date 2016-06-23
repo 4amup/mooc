3d612dbad71f7df1ddcb4b271d2066326cbd7519
wozhizuipc <wozhizui1990@163.com>
2016-06-23 19:03:28 +0800

    add function getHTTPObject
    兼容各种浏览器的得到XMLHttpRequest对象的方法。

--- Web Dev/DOM Scripting/chapter12/web dev example/scripts/global.js
+++ Web Dev/DOM Scripting/chapter12/web dev example/scripts/global.js
@@ -351,6 +351,20 @@ function validateForm(whichform) {
   }
   return true;
 }
+function getHTTPObject() {
+  if (typeof XMLHttpRequest == "undefined") {
+    XMLHttpRequest = function () {
+      try {return new ActiveXObject("Msxml2.XMLHTTP.6.0");}
+        catch (e) {}
+      try {return new ActiveXObject("Msxml2.XMLHTTP.3.0");}
+        catch (e) {}
+      try {return new ActiveXObject("Msxml2.XMLHTTP");}
+        catch (e) {}
+      return false;
+    }
+  }
+  return new XMLHttpRequest();
+}
 addLoadEvent(focusLabels);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(highLightPage);
