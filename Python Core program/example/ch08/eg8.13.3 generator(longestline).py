dbc59b5bbb52f28400f42aa344ae7fa5aa1be9f3
Maclixiaochun <wozhizui1990@163.com>
2016-09-01 21:19:32 +0800

    change the structure of code

--- Python Core program/example/ch08/eg8.13.3 generator(longestline).py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-f = open('motd.txt')
-longest = max(len(x.strip())for x in f)
-f.close()
-print longest
--- Python Core program/example/ch09/safe_float_func.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-def safe_float(obj):
-	try:
-		reval = float(obj)
-	except (ValueError, TypeError), diag:
-		reval = str(diag)
-	return reval
