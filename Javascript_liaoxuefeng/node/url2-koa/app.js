7df868bceeed8f6af12af6f0642d6c117f26ba30
wozhizuipc <wozhizui1990@163.com>
2016-09-02 09:39:38 +0800

    add koa2
    重构了一下koa的登陆，更加模块化了。
    不是太懂，还需要后续看书强化。

--- Javascript_liaoxuefeng/node/url2-koa/app.js
+++ Javascript_liaoxuefeng/node/url2-koa/app.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
 
+const bodyParser = require('koa-bodyparser');
+
 const router = require('koa-router')();
 
-const bodyParser = require('koa-bodyparser');
+const fs = require('fs');
 
 const app = new Koa();
 
@@ -12,29 +14,41 @@ app.use(async (ctx, next) => {
     await next();
 });
 
+// parse request body:
 app.use(bodyParser());
-// add url-route:
-router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
-        <form action="/signin" method="post">
-            <p>Name: <input name="name" value="koa"></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="Submit"></p>
-        </form>`;
-});
 
-router.post('/signin', async (ctx, next) => {
-    var
-        name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
+// add url-route in /controllers:
+
+function addMapping(router, mapping) {
+    for (var url in mapping) {
+        if (url.startsWith('GET ')) {
+            var path = url.substring(4);
+            router.get(path, mapping[url]);
+            console.log(`register URL mapping: GET ${path}`);
+        } else if (url.startsWith('POST ')) {
+            var path = url.substring(5);
+            router.post(path, mapping[url]);
+            console.log(`register URL mapping: POST ${path}`);
+        } else {
+            console.log(`invalid URL: ${url}`);
+        }
     }
-});
+}
+
+function addControllers(router) {
+    var files = fs.readdirSync(__dirname + '/controllers');
+    var js_files = files.filter((f) => {
+        return f.endsWith('.js');
+    }, files);
+
+    for (var f of js_files) {
+        console.log(`process controller: ${f}...`);
+        let mapping = require(__dirname + '/controllers/' + f);
+        addMapping(router, mapping);
+    }
+}
+
+addControllers(router);
 
 // add router middleware:
 app.use(router.routes());
