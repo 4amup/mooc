f0de1fac5009def22be818922f7c7d4a7d4d7e3a
wozhizuipc <wozhizui1990@163.com>
2016-09-02 15:29:13 +0800

    update env

--- Javascript_liaoxuefeng/node/use-nunjucks/app.js
+++ Javascript_liaoxuefeng/node/use-nunjucks/app.js
@@ -1,28 +1,36 @@
 const nunjucks = require('nunjucks');
-
-function createEnv(path,opts){
-  var
-    autoescape = opts.autoescape && true,
-    noCahe = opts.noCache || false,
-    watch = opts.watch || false,
-    throwOnUnderdefined = opts.throwOnUnderdefined || false,
-    env = new nunjucks.Environment(
-      new nunjuck.FileSystemLoader('views',{
-        noCache: noCahe,
-        watch: watch
-      }),{
-        autoescape: autoescape,
-        throwOnUndefined: throwOnUndifined
-      });
-    
+// 建立一个environment对象的构造函数
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape && true,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
     if (opts.filters) {
-      for (var f in opts.filters) {
-        env.addFilter(f,opts.filters[f]);        
-      }
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
     }
     return env;
 }
-
+// 创建一个environment的实例
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
+// 创建environment实例
 var env = createEnv('view',{
   watch:true,
   filters:{
@@ -31,3 +39,5 @@ var env = createEnv('view',{
     }
   }
 });
+var s = env.render('hello.html',{name:'小明'});
+console.log(s);
