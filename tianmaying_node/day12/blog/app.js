c953e452495dec7008d81d0a438fb11d94bddce2
Maclixiaochun <wozhizui1990@163.com>
2016-10-10 23:15:53 +0800

    update day12 connect db

--- tianmaying_node/day12/blog/app.js
+++ tianmaying_node/day12/blog/app.js
@@ -5,6 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('hbs');
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://127.0.0.1/test');
+
+// 数据库测试代码
+var Cat = mongoose.model('Cat', { name: String});
+
+var kitty = new Cat({ name: 'mimi' });
+kitty.save(function (err) {
+  if (err) // ...
+  console.log('meow');
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
