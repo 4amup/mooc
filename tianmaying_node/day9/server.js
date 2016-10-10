e3fc872ac483bae229ad8bd54bdd23588f3f7a9a
Maclixiaochun <wozhizui1990@163.com>
2016-10-10 11:56:58 +0800

    update day9 over morgan

--- tianmaying_node/day9/server.js
+++ tianmaying_node/day9/server.js
@@ -1,7 +1,14 @@
+var fs = require('fs')
 var express = require('express');
 var favicon = require('serve-favicon');
+var logger = require('morgan');
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'}); 
+//以上flags：a的意思是以追加模式打开文件，不存在则创建
+
+
 var app = express();
 app.use(favicon(__dirname + '/favicon.ico'));
+app.use(logger('combined', {stream: accessLogStream}));
 
 app.get('/', function(req, res){
   res.send('hello world');
