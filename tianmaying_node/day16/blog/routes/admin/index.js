500787c008ced74b00427020f7699b563af27ad7
lixiaochunpc <490398311@qq.com>
2016-10-21 16:42:17 +0800

    over day16

--- tianmaying_node/day16/blog/routes/admin/index.js
+++ tianmaying_node/day16/blog/routes/admin/index.js
@@ -2,5 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/profile', require('./profile'));
+router.use('/password', require('./password'));
+router.use('/post', require('./post'));
 
 module.exports = router;
