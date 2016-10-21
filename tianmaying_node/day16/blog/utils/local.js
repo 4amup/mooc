2297b4c715cada4be8ec36b48690a0630efd2129
lixiaochunpc <490398311@qq.com>
2016-10-21 15:45:20 +0800

    day 16 update

---
+++ tianmaying_node/day16/blog/utils/local.js
@@ -0,0 +1,10 @@
+var cfg = require('../config');
+
+module.exports = function (req, res, next) {
+
+    res.locals.req = req;
+    res.locals.res = res;
+    res.locals.config = cfg;
+
+    next();
+};
