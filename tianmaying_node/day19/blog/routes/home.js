9a55a905085bd7262647a1e577b8fc008ba65f8c
lixiaochunpc <490398311@qq.com>
2016-10-26 15:10:56 +0800

    day 19 update

--- tianmaying_node/day19/blog/routes/home.js
+++ tianmaying_node/day19/blog/routes/home.js
@@ -1,11 +1,8 @@
-/**
- * Created by harttle on 15/4/14.
- */
 var router = require('express').Router();
 var Post = require('../models/post.js');
 var authRequired = require('../utils/auth-required');
 var User = require('../models/user.js');
-// require('mongoose-query-paginate');
+require('mongoose-query-paginate');
 
 
 router.get('/', function(req, res, next) {
@@ -15,6 +12,35 @@ router.get('/', function(req, res, next) {
             users: users,
             title: 'TEST BLOG: 一个简单的博客系统'
         });
+    });
+});
+
+router.get('/home', authRequired, function(req, res, next) {
+    res.redirect('/user/' + req.user.id);
+});
+
+router.get('/user/:id', function (req, res, next) {
+    User.findById(req.params.id, function (err, author) {
+        if (err || !author) return next(err);
+
+        var cond = {author: req.params.id};
+
+        var options = {
+            perPage: 5,
+            delta: 2,
+            page: req.query.page || 1
+        };
+
+        Post.find(cond)
+            .sort({_id: -1})
+            .paginate(options, function (err, pager) {
+                if (err) return next(err);
+                res.render('home/user', {
+                    pager: pager,
+                    title: author.username + ' 的首页',
+                    author: author
+                });
+            });
     });
 });
 
