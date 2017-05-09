91c29749afd95266b1d2554fe6c67e4544ca84c1
wozhizuipc <490398311@qq.com>
2017-05-09 12:24:19 +0800

    进一步抽象了路由函数

--- JavaScript_2nd_liao/url-koa-rec/app.js
+++ JavaScript_2nd_liao/url-koa-rec/app.js
@@ -14,60 +14,39 @@ app.use(async (ctx, next) => {
     await next();
 });
 
-// 先导入fs模块，然后用同步读取文件，因为只启动一次同步，不存在性能问题
-var files = fs.readdirSync(__dirname + '/controllers');
-// 过滤出.js文件，其中endswith()方法是es6开始原生支持的
-var js_files = files.filter((f)=>{
-    return f.endsWith('.js');
-});
-
-// 开始处理每个js文件，js_files是一个数组
-for(var f of js_files) {
-    console.log(`process controller: ${f}...`);
-    // 导入js文件
-    let mapping = require(__dirname + '/controllers/' + f);
-    for (let url in mapping) {
-        if (url.startsWith('GET')) {
-            // 如果url类似于"GET xxx";
+// 进一步抽象路由函数
+function addMapping(router, mapping) {
+    for(let url in mapping) {
+        if(url.startsWith('GET')) {
             let path = url.substring(4);
             router.get(path, mapping[url]);
             console.log(`register URL mapping: GET ${path}`);
-        } else if(url.startsWith('POST')) {
+        } else if (url.startsWith('POST')) {
             let path = url.substring(4);
             router.post(path, mapping[url]);
             console.log(`register URL mapping: POST ${path}`);
         } else {
-            // 无效的URL
             console.log(`invalid URL: ${url}`);
         }
     }
 }
 
+function addControllers(router) {
+    let files = fs.readdirSync(__dirname + '/controllers/');
+    let js_files = files.filter((f)=> {
+        return f.endsWith('.js');
+    });
+
+    for (let f of js_files) {
+        console.log(`process controller: ${f}...`);
+        let mapping = require(__dirname + '/controllers/' + f);
+        addMapping(router, mapping);
+    }
+
+}
+addControllers(router);
 // add router middleware:
 app.use(router.routes());
-
-// // add url-route:
-// router.get('/', async (ctx, next) => {
-//     ctx.response.body = `<h1>Index</h1>
-//         <form action="/signin" method="post">
-//             <p>Name: <input name="name" value="koa"></p>
-//             <p>Password: <input name="password" type="password"></p>
-//             <p><input type="submit" value="Submit"></p>
-//         </form>`;
-// });
-
-// router.post('/signin', async (ctx, next) => {
-//     var name = ctx.request.body.name || '',
-//         password = ctx.request.body.password || '';
-
-//     console.log(`signin with name: ${name}, password: ${password}`);
-//     if(name === 'koa' && password === "12345") {
-//         ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-//     } else {
-//         ctx.response.body = `<h1>Login faild!</h1>
-//         <p><a href="/">Try again.</a></p>`;
-//     }
-// });
 
 app.listen(3000);
 console.log('app started at port 3000...');
