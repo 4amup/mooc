cfe7c1e62ea755ae2991afc83c5173ad32293302
wozhizuipc <490398311@qq.com>
2017-05-09 14:16:20 +0800

    再次优化，自定义了controller中间件

--- JavaScript_2nd_liao/url-koa-rec/app.js
+++ JavaScript_2nd_liao/url-koa-rec/app.js
@@ -14,39 +14,8 @@ app.use(async (ctx, next) => {
     await next();
 });
 
-// 进一步抽象路由函数
-function addMapping(router, mapping) {
-    for(let url in mapping) {
-        if(url.startsWith('GET')) {
-            let path = url.substring(4);
-            router.get(path, mapping[url]);
-            console.log(`register URL mapping: GET ${path}`);
-        } else if (url.startsWith('POST')) {
-            let path = url.substring(4);
-            router.post(path, mapping[url]);
-            console.log(`register URL mapping: POST ${path}`);
-        } else {
-            console.log(`invalid URL: ${url}`);
-        }
-    }
-}
-
-function addControllers(router) {
-    let files = fs.readdirSync(__dirname + '/controllers/');
-    let js_files = files.filter((f)=> {
-        return f.endsWith('.js');
-    });
-
-    for (let f of js_files) {
-        console.log(`process controller: ${f}...`);
-        let mapping = require(__dirname + '/controllers/' + f);
-        addMapping(router, mapping);
-    }
-
-}
-addControllers(router);
 // add router middleware:
-app.use(router.routes());
+app.use(require('./controllers')());
 
 app.listen(3000);
 console.log('app started at port 3000...');
