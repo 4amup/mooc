53c8c68a2449bab0314ca75c67a8c609d56c4fef
wozhizuipc <490398311@qq.com>
2017-05-09 16:37:05 +0800

    简单是用nunjucks模板引擎

---
+++ JavaScript_2nd_liao/use-nunjucks/app.js
@@ -0,0 +1,34 @@
+const nunjucks = require('nunjucks');
+
+function createEnv(path, opts) {
+    let autoescape = opts.autoescape && true,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if(opts.filters) {
+        for(let f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
+
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function(n) { // 这个过滤器用来将转换成16位数字
+            return 'Ox' + n.toString(16);
+        }
+    }
+});
+
+var s = env.render('hello.html', {name: '小明'});
+console.log(s);
