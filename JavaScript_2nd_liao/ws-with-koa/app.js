626ff5620fb121b0049e3da936d18d41e7295c97
wozhizuipc <490398311@qq.com>
2017-05-20 17:35:41 +0800

    over WebSocket 部分小属性和教程相比，有小改动

--- JavaScript_2nd_liao/ws-with-koa/app.js
+++ JavaScript_2nd_liao/ws-with-koa/app.js
@@ -1,3 +1,9 @@
+const url = require('url');
+
+const ws = require('ws');
+
+const Cookies = require('cookies');
+
 const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
@@ -6,33 +12,148 @@ const controller = require('./controller');
 
 const templating = require('./templating');
 
+const WebSocketServer = ws.Server;
+
 const app = new Koa();
 
 const isProduction = process.env.NODE_ENV === 'production'; // 在这里写错了，这是个判断的，我当成了赋值的，找了半天才找到错误
-
-// 记录URL以及页面执行时间
-app.use(async(ctx, next) => {
-    console.log(`Process ${ctx.request.method}${ctx.request.url}`);
-    let start = new Date().getTime(),
-        execTime;
+// log request URL
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
-    execTime = new Date().getTime() - start;
-    ctx.response.set('X-Response-Time', `${execTime}ms`);
+});
+
+// parse user from cookie
+app.use(async (ctx, next) => {
+    // parserUser在下面声明
+    ctx.state.user = parseUser(ctx.cookies.get('name') || '');
+    await next();
 })
-// 处理静态文件的中间件
+
+// 解析静态文件
 if(!isProduction) {
     let staticFiles = require('./static-files');
     app.use(staticFiles('/static/', __dirname +'/static'));
 }
-// 解析POST请求的内容
+
+// parser request body
 app.use(bodyParser());
-// 负责给ctx加上render()来使用Nunjucks
+
+// add nunjucks as view
 app.use(templating('views', {
-    noCache: !isProduction,
-    watch: !isProduction
-}));
+    noCache: true,
+    watch: true
+}))
+
 // 处理URL路由
 app.use(controller());
+// koa app的listen()方法返回http.Server
+let server = app.listen(3000);
 
-app.listen(3000);
+// 解析user的函数
+function parseUser(obj) {
+    if(!obj) return;
+    console.log('try parse: ' + obj);
+    let s = '';
+    if(typeof obj === 'string') {
+        s = obj;
+    } else if(obj.headers) {
+        let cookies = new Cookies(obj, null);
+        s = cookies.get('name');
+    }
+    if(s) {
+        try {
+            let user = JSON.parse(Buffer.from(s, 'base64').toString());
+            console.log(`User: ${user.name}, ID: ${user.id}`);
+            return user;
+        } catch (err) {
+            // ignore
+        }
+    }
+}
+// 创建WebSocketServer
+function createWebSocketServer(server, onConnection, onMessage, onClose, onError) {
+    let wss = new WebSocketServer({
+        server: server
+    });
+    wss.broadcast = function broadcast(data) {
+        wss.clients.forEach(function each(client) {
+            client.send(data);
+        });
+    };
+    onConnection = onConnection || function () {
+        console.log('[WebSocket] connected.');
+    };
+    onMessage = onMessage || function (msg) {
+        console.log('[WebSocket] message received: ' + msg);
+    };
+    onClose = onClose || function (code, message) {
+        console.log(`[WebSocket] closed: ${code} - ${message}`);
+    };
+    onError = onError || function (err) {
+        console.log('[WebSocket] error: ' + err);
+    };
+    wss.on('connection', function (ws, req) {
+        let location = url.parse(req.url, true);// upgradeReq属性已经被ws删掉了
+        console.log('[WebSocketServer] connection: ' + location.href);
+        ws.on('message', onMessage);
+        ws.on('close', onClose);
+        ws.on('error', onError);
+        if (location.pathname !== '/ws/chat') {
+            // close ws:
+            ws.close(4000, 'Invalid URL');
+        }
+        // check user:
+        let user = parseUser(req);
+        if (!user) {
+            ws.close(4001, 'Invalid user');
+        }
+        ws.user = user;
+        ws.wss = wss;
+        onConnection.apply(ws);
+    });
+    console.log('WebSocketServer was attached.');
+    return wss;
+}
+
+var messageIndex = 0;
+
+function createMessage(type, user, data) {
+    messageIndex ++;
+    return JSON.stringify({
+        id: messageIndex,
+        type: type,
+        user: user,
+        data: data
+    });
+}
+
+function onConnect() {
+    let user = this.user;
+    let msg = createMessage('join', user, `${user.name} joined.`);
+    this.wss.broadcast(msg);
+    // build user list:
+    let users = [];
+    this.wss.clients.forEach(function (client) {
+        users.push(client.user);
+    });
+    this.send(createMessage('list', user, users));
+}
+
+function onMessage(message) {
+    console.log(message);
+    if (message && message.trim()) {
+        let msg = createMessage('chat', this.user, message.trim());
+        this.wss.broadcast(msg);
+    }
+}
+
+function onClose() {
+    let user = this.user;
+    let msg = createMessage('left', user, `${user.name} is left.`);
+    this.wss.broadcast(msg);
+}
+
+app.wss = createWebSocketServer(server, onConnect, onMessage, onClose);
+
 console.log('listenning at http://localhost:3000');
--- JavaScript_2nd_liao/ws-with-koa/controllers/index.js
+++ JavaScript_2nd_liao/ws-with-koa/controllers/index.js
@@ -1,7 +1,12 @@
 module.exports = {
     'GET /': async (ctx, next) => {
-        ctx.render('index.html', {
-            title: 'Welcome'
-        });
+        let user = ctx.state.user;
+        if(user) {
+            ctx.render('room.html', {
+                user: user
+            });
+        } else {
+            ctx.response.redirect('/signin');
+        }
     }
 }
